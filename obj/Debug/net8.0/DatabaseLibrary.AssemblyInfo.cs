da2c2755c6f404ce42f49c5c94d2f810a267c7e6
tiosatria <azu818181@gmail.com>
2024-04-06 23:45:16 +0800

    user crud

--- obj/Debug/net8.0/DatabaseLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/DatabaseLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a62e9e0814eddd9cbb0700787109fbbc2cedba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae2a8ac9a89ea119e11b090239e6120ee1d72e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
